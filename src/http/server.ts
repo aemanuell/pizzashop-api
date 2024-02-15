cf6b76701339ad7e20b2c5c26089686993ca1d3a
aemanuell <antonioemanueell@gmail.com>
2024-02-15 17:54:51 -0300

    Initial commit

---
+++ src/http/server.ts
@@ -0,0 +1,41 @@
+import { Elysia, t } from 'elysia'
+import { restaurants, users } from '../db/schema'
+import { db } from '../db/connection'
+
+const app = new Elysia().post('/restaurants', async ({ body, set }) => {
+    const { restaurantName, managerName, email, phone } = body as any
+  
+    const [manager] = await db
+      .insert(users)
+      .values({
+        name: managerName,
+        email,
+        phone,
+        role: 'manager',
+      })
+      .returning({
+        id: users.id,
+      })
+  
+    await db.insert(restaurants).values({
+      name: restaurantName,
+      managerId: manager.id,
+    })
+  
+    set.status = 204
+  },
+  
+  {
+    body: t.Object({
+      restaurantName: t.String(),
+      managerName: t.String(),
+      phone: t.String(),
+      email: t.String({ format: 'email' }),
+    }),
+  }
+
+)
+  
+app.listen(3333, () => {
+  console.log('🔥 HTTP server running!')
+})
