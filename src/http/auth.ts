0904361b5daa38d34d4835eb9f05f0dd8fef8033
aemanuell <antonioemanueell@gmail.com>
2024-03-03 17:42:48 -0300

    Rota: Perfil do usuário logado, Lidando com erros no Elysia, Restaurante gerenciado

--- src/http/auth.ts
+++ src/http/auth.ts
@@ -2,6 +2,7 @@ import cookie from '@elysiajs/cookie'
 import jwt from '@elysiajs/jwt'
 import { Elysia, t, type Static } from 'elysia'
 import { env } from '../env'
+import { UnauthorizedError } from './erros/unauthorized-error'
 
 const jwtPayload = t.Object({
   sub: t.String(),
@@ -9,6 +10,20 @@ const jwtPayload = t.Object({
 })
 
 export const auth = new Elysia()
+  .error({
+    UNAUTHORIZED: UnauthorizedError,
+  })
+  .onError(({ error, code, set }) => {
+    switch (code) {
+      case 'UNAUTHORIZED': {
+        set.status = 401
+        return {
+          code,
+          message: error.message,
+        }
+      }
+    }
+  })
   .use(
     jwt({
       secret: env.JWT_SECRET_KEY,
@@ -16,7 +31,7 @@ export const auth = new Elysia()
     }),
   )
   .use(cookie())
-  .derive(({ jwt, setCookie, removeCookie }) => {
+  .derive(({ jwt, setCookie, removeCookie, cookie }) => {
     return {
       signUser: async (payload: Static<typeof jwtPayload>) => {
         const token = await jwt.sign(payload)
@@ -30,6 +45,19 @@ export const auth = new Elysia()
 
       signOut: () => {
         removeCookie('auth')
+      },
+
+      getCurrentUser: async () => {
+        const payload = await jwt.verify(cookie.auth)
+
+        if (!payload) {
+          throw new UnauthorizedError()
+        }
+
+        return {
+          userId: payload.sub,
+          restaurantId: payload.restaurantId,
+        }
       },
     }
   })
--- src/http/server.ts
+++ src/http/server.ts
@@ -4,12 +4,30 @@ import { registerRestaurante } from './routes/register-restaurante'
 import { sendAuthLink } from './routes/send-auth-link'
 import { authenticateFromLink } from './routes/authenticate-from-link'
 import { signOut } from './routes/sign-out'
+import { getProfile } from './routes/get-profile'
+import { getManagedRestaurante } from './routes/get-managed-restaurant'
 
 const app = new Elysia()
   .use(registerRestaurante)
   .use(sendAuthLink)
   .use(authenticateFromLink)
   .use(signOut)
+  .use(getProfile)
+  .use(getManagedRestaurante)
+  .onError(({ code, error, set }) => {
+    switch (code) {
+      case 'VALIDATION': {
+        set.status = error.status
+
+        return error.toResponse()
+      }
+      default: {
+        console.error(error)
+
+        return new Response(null, { status: 500 })
+      }
+    }
+  })
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server running!')
